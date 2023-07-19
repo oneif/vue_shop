106e77be964c7b595ab2d88c22219deb7584a22f
if <if17825578404@126.com>
2023-07-19 17:00:10 +0800

    finish login

--- src/main.js
+++ src/main.js
@@ -2,7 +2,19 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+// 导入全局样式表
+import './assets/css/global.css'
+// 导入elementUI
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css';
+// 二次封装axios
+import api from './api/index.js'
+
+
+
 Vue.config.productionTip = false
+Vue.use(ElementUI)
+Vue.prototype.$api = api
 
 new Vue({
   router,
---
+++ src/utils/request.js
@@ -0,0 +1,6 @@
+import axios from "axios";
+
+export default axios.create({
+    baseURL:'http://127.0.0.1:8888/api/private/v1',
+    timeout: 5000,
+})
